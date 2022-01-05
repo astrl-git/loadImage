a18f856cad5dd7396972fdf398b882a4b34a641f
astrl-git <strilchuk.work@gmail.com>
2022-01-06 00:26:06 +0100

    feat: done, implement images loading from galery, images removing, refreshing the list

---
+++ Assets/Scripts/PngPreview/PngImagesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace PngPreview
+{
+    public class PngImagesController : MonoBehaviour
+    {
+        private static TimeSpan _1_SEC = new TimeSpan(0, 0, 1);
+
+        [SerializeField] private Transform listContainer;
+        [SerializeField] private PngPreviewItem itemPrefab;
+        [SerializeField] private Button addItem;
+        [SerializeField] private Button refreshList;
+
+        private Dictionary<string, PngPreviewItem> spawnedItems;
+
+        public IObservable<long> Timer { get; private set; }
+
+        private void OnValidate()
+        {
+            Assert.IsNotNull(itemPrefab, $"{nameof(itemPrefab)} is null");
+            Assert.IsNotNull(addItem, $"{nameof(addItem)} is null");
+            Assert.IsNotNull(refreshList, $"{nameof(refreshList)} is null");
+        }
+
+        private void Awake()
+        {
+            spawnedItems = new Dictionary<string, PngPreviewItem>();
+            Timer = Observable.Interval(_1_SEC);
+        }
+
+        private void OnEnable()
+        {
+            addItem.onClick.AsObservable().TakeUntilDisable(gameObject).Subscribe(i => UploadImageFromGallery());
+            refreshList.onClick.AsObservable().TakeUntilDisable(gameObject).Subscribe(i => RefreshList());
+        }
+
+        public void RemoveItem(string itemPath)
+        {
+            if (spawnedItems.TryGetValue(itemPath, out var item))
+            {
+                Debug.Log($"RemovedItem: {itemPath}");
+                Destroy(item.gameObject);
+                spawnedItems.Remove(itemPath);
+            }
+        }
+
+        private void UploadImageFromGallery()
+        {
+            NativeGallery.GetImageFromGallery((imagePath) =>
+            {
+                Debug.Log($"Image path: {imagePath}");
+                if (imagePath != null)
+                {
+                    var data = LoadPNGImage(imagePath);
+
+                    SpawnItem(data);
+                }
+            });
+        }
+
+        private void RefreshList()
+        {
+            var pathList = spawnedItems.Select(i => i.Key).ToArray();
+            foreach (var path in pathList)
+            {
+                RemoveItem(path);
+            }
+
+            foreach (var path in pathList)
+            {
+                LoadImageByPath(path);
+            }
+        }
+
+        private void LoadImageByPath(string imagePath)
+        {
+            NativeGallery.Permission permission = NativeGallery.RequestPermission(NativeGallery.PermissionType.Read);
+
+            if (permission != NativeGallery.Permission.Granted)
+            {
+                Debug.LogError($"Failed to load file data due to permission is {imagePath}");
+                return;
+            }
+
+            var data = LoadPNGImage(imagePath);
+
+            SpawnItem(data);
+        }
+
+        private void SpawnItem(PngPreviewItem.ImageData data)
+        {
+            // var data = new PngPreviewItem.ImageData(imagePath, texture, DateTime.Now);
+
+            if (spawnedItems.TryGetValue(data.fullPath, out var alreadySpawnedItem))
+            {
+                alreadySpawnedItem.UpdateData(data);
+                Debug.Log($"UpdatedItem: {data.fullPath}");
+            }
+            else
+            {
+                var listItem = Instantiate(itemPrefab, listContainer);
+                listItem.Init(data, this);
+
+                spawnedItems.Add(data.fullPath, listItem);
+                Debug.Log($"SpawnedItem: {data.fullPath}");
+            }
+        }
+
+        private PngPreviewItem.ImageData LoadPNGImage(string imagePath)
+        {
+            // TODO UNCOMMENT TO LOAD ONLY .PNG
+            // var extension = Path.GetExtension(imagePath).ToLowerInvariant();
+            // if (extension != ".png")
+            // {
+            //     Debug.LogError($"Failed to load file due to it's extension is not a .png");
+            //     return null;
+            // }
+
+            if (File.Exists(imagePath) == false)
+            {
+                Debug.LogError($"Failed to load file due to it doesn't exist");
+                return null;
+            }
+
+            var fileData = File.ReadAllBytes(imagePath);
+            var texture = new Texture2D(2, 2);
+            texture.LoadImage(fileData);
+
+            var createdDate = File.GetCreationTime(imagePath);
+            return new PngPreviewItem.ImageData(imagePath, texture, createdDate.ToUniversalTime());
+        }
+    }
+}
---
+++ Assets/Scripts/PngPreview/PngPreviewItem.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Linq;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace PngPreview
+{
+    public class PngPreviewItem : MonoBehaviour
+    {
+        const string FORMAT = @"d\D\a\y\s\,\ hh\:mm\:ss";
+
+        public class ImageData
+        {
+            public Texture2D texture;
+            public string fullPath;
+            public string fileName;
+            public DateTime createdDate;
+
+            public ImageData(string fullPath, Texture2D texture, DateTime createdDate)
+            {
+                this.fullPath = fullPath;
+                this.texture = texture;
+                this.createdDate = createdDate;
+                fileName = fullPath.Split('/').Last();
+            }
+        }
+
+        [SerializeField] private Image previewImage;
+        [SerializeField] private TextMeshProUGUI fileName;
+        [SerializeField] private TextMeshProUGUI createdTimer;
+        [SerializeField] private Button removeItemButton;
+
+        private ImageData _data;
+        private PngImagesController _controller;
+
+        private void OnValidate()
+        {
+            Assert.IsNotNull(previewImage, $"{nameof(previewImage)} is null");
+            Assert.IsNotNull(fileName, $"{nameof(fileName)} is null");
+            Assert.IsNotNull(createdTimer, $"{nameof(createdTimer)} is null");
+            Assert.IsNotNull(removeItemButton, $"{nameof(removeItemButton)} is null");
+        }
+
+        private void Awake()
+        {
+            removeItemButton.onClick
+                .AsObservable()
+                .TakeUntilDisable(gameObject)
+                .Subscribe(i =>
+                {
+                    if (string.IsNullOrEmpty(_data?.fullPath))
+                    {
+                        Debug.LogError($"Failed to remove list item due to missed image path data");
+                        return;
+                    }
+
+                    if (_controller != null) _controller.RemoveItem(_data?.fullPath);
+                });
+        }
+
+        public void Init(ImageData data, PngImagesController controller)
+        {
+            if (data == null || controller == null)
+            {
+                Debug.LogError($"Failed to Init {nameof(PngPreviewItem)} due to args is/are null");
+                return;
+            }
+
+            _data = data;
+            _controller = controller;
+            _controller.Timer.TakeUntilDisable(gameObject).Subscribe(Tick);
+
+            SetUIAccordingToData(_data);
+        }
+
+        public void UpdateData(ImageData data)
+        {
+            if (data == null)
+            {
+                Debug.LogError($"Failed to UpdateData due to arg is null");
+                return;
+            }
+
+            _data = data;
+            SetUIAccordingToData(_data);
+        }
+
+        private void Tick(long _)
+        {
+            if (_data?.createdDate == default) return;
+
+            var utcNow = DateTime.UtcNow;
+            var timespan = utcNow - _data.createdDate.ToUniversalTime();
+            var res = timespan.ToString(FORMAT);
+
+            if (_ % 10 == 0) Debug.Log($"utcNow: {utcNow} - _data.CreatedDate.ToUniversalTime(): {_data.createdDate.ToUniversalTime()} = res:{res}");
+            createdTimer.text = res;
+        }
+
+        private Sprite TextureToSprite(Texture2D texture) => Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2);
+
+        private void SetUIAccordingToData(ImageData data)
+        {
+            if (data.texture == null)
+            {
+                Debug.LogError($"Failed to display an image due to texture data is null");
+            }
+            else
+            {
+                previewImage.sprite = TextureToSprite(data.texture);
+            }
+
+            fileName.text = data.fileName;
+        }
+    }
+}
